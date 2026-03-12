876ef52b05c88efa7aef3b6fd7741d7f9399ae2b
Luiz Barcelar <luizbarcelar2014@gmail.com>
2026-03-12 16:08:57 -0300

    fix: allow CORS in next.config.mjs

--- BackEnd/next.config.mjs
+++ BackEnd/next.config.mjs
@@ -1,19 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async headers() {
-        return [
-            {
-                // matching all API routes
-                source: "/api/:path*",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "http://localhost:5173" },
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Authorization, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-                ]
-            }
+  async headers() {
+    return [
+      {
+        // Aplica estes cabeçalhos para todas as rotas de API
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // Permite qualquer origem (ideal para projetos de portfólio)
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ]
-    }
+      }
+    ]
+  }
 };
 
-export default nextConfig;
+export default nextConfig
